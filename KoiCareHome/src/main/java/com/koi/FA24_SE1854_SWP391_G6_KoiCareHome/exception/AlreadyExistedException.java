cce8ca64ffa7103d26e049fa68bdf626ada1ba64
Anhqt20 <anhqtse183983@fpt.edu.vn>
2024-10-15 10:26:03 +0700

    clear

--- KoiCareHome/src/main/java/com/koi/FA24_SE1854_SWP391_G6_KoiCareHome/exception/AlreadyExistedException.java
+++
@@ -1,7 +0,0 @@
-package com.koi.FA24_SE1854_SWP391_G6_KoiCareHome.exception;
-
-public class AlreadyExistedException extends RuntimeException {
-    public AlreadyExistedException(String message) {
-        super(message);
-    }
-}
--- KoiCareHome/src/main/java/com/koi/FA24_SE1854_SWP391_G6_KoiCareHome/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.koi.FA24_SE1854_SWP391_G6_KoiCareHome.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
