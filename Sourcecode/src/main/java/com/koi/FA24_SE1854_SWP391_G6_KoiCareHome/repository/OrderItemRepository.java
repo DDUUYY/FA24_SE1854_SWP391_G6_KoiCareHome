894250daa1fc680102ef90725f917aadf23734e2
Anhqt20 <anhqtse183983@fpt.edu.vn>
2024-11-21 19:44:11 +0700

    add fish validation, add growth rate for food calculator

--- Sourcecode/src/main/java/com/koi/FA24_SE1854_SWP391_G6_KoiCareHome/repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.koi.FA24_SE1854_SWP391_G6_KoiCareHome.repository;
-
-import com.koi.FA24_SE1854_SWP391_G6_KoiCareHome.model.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
-}
